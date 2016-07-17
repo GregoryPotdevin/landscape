ba0c08c57887427c73431a08eeb20040698e2fc0
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-07-17 22:15:14 +0200

    test static builds

---
+++ webpack.prod.config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool:"eval",
+  context:path.join(__dirname),
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react')
+    },
+    extensions:[".js", ".jsx", ".webpack.js", ".web.js",""]
+  },
+  resolveLoader: {
+    root: path.join(__dirname, "node_modules")
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: [
+            require.resolve('babel-preset-es2015'),
+            require.resolve('babel-preset-react'),
+            require.resolve('babel-preset-react-hmre'),
+            require.resolve('babel-preset-stage-0')
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style", "css"]
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loaders: [
+            'file-loader?name=[path][name].[ext]'
+        ]
+      }
+    ]
+  }
+};
